c388c0860836af0dbf71f7b1d8fe438eee13e0d6
Quentin Bramas <bramas@unistra.fr>
2020-04-29 17:07:00 +0200

    Initial Commit

---
+++ FileHanger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileHanger
+//
+//  Created by bramas on 29/04/2020.
+//  Copyright © 2020 bramas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
